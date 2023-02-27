e76f259494987d56b47a3e1b85fe318952c98e27
Ming <zhanm85@mcmaster.ca>
2023-02-27 13:00:04 -0500

    20230227_update the chinese transmission

--- day_6_methods/socket_client.py
+++ day_6_methods/socket_client.py
@@ -3,9 +3,12 @@ import socket
 client = socket.socket() # 声明socket类型，同时生成socket连接对象；default is enough
 client.connect(('localhost', 6969))
 
-client.send(b"Hello World!")
+# client.send(b"Hello World!")
+client.send("如果是中文的话，需要转码 abc".encode("utf-8"))
 
 data = client.recv(1024)    # 收的长度为X字节
-print(f"Recv: {data}")
+# print(f"Recv: {data}")
+# 同样的，如果接收的是中文的话，需要解码
+print(f"Recv: {data.decode()}")
 
 client.close()
--- day_6_methods/socket_server.py
+++ day_6_methods/socket_server.py
@@ -12,7 +12,10 @@ print(f"Conn: {conn}\n Addr: {addr}")
 
 print("The link is connected.")
 data = conn.recv(1024)    # 接收发送端的数据
-print(f"Recv: {data}")
+# 如果是中文的接收，需要解码
+# print(f"Recv: {data}")
+print(f"Recv: {data.decode()}")
+
 
 conn.send(data.upper())   # 发送数据给发送端
 
